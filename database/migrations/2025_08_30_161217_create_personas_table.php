8aa3ec9e2754c1aa5c4deb4c9ef5b806b8cdd98a
tomas09208 <psfaxboss@gmail.com>
2025-08-30 12:09:17 -0500

    primera parte

---
+++ database/migrations/2025_08_30_161217_create_personas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('razon_social',80);
+            $table->string('direcion',80);
+            $table->string('tipo_persona',20);
+             $table->TinyInteger('estado',20)->default(1);
+
+
+             
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('template');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/401', function () {
+    return view('pages.401');
+});
+
+Route::get('/404', function () {
+    return view('pages.404');
+});
+
+Route::get('/500', function () {
+    return view('pages.500');
+});
